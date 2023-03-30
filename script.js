e45206821d302773516e6b03fb2ec3201da088dd
Jacob Softleigh-Moore <Jmsm1g19@soton.ac.uk>
2023-03-30 20:38:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,146 @@
+document.getElementById("addBtn").addEventListener("click", addTask);
+document.getElementById("editBtn").addEventListener("click", toggleEditMode);
+document.getElementById("todoInput").addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+}
+let editMode = false;
+
+function toggleEditMode() {
+    editMode = !editMode;
+    document.getElementById("editBtn").textContent = editMode ? "Finish Editing" : "Edit Tasks";
+}
+
+function addTask() {
+    const input = document.getElementById("todoInput");
+    const noteInput = document.getElementById("noteInput");
+    const dueDateInput = document.getElementById("dueDateInput");
+    const task = input.value.trim();
+    const note = noteInput.value.trim();
+    const dueDate = dueDateInput.value;
+
+    if (task.length > 0) {
+        const li = document.createElement("li");
+
+        const taskContainer = document.createElement("div");
+        taskContainer.classList.add("task-content");
+
+        const taskContent = document.createElement("span");
+        taskContent.textContent = task;
+        taskContainer.appendChild(taskContent);
+        li.appendChild(taskContainer);
+
+        if (dueDate) {
+            const dueDateContent = document.createElement("span");
+            dueDateContent.textContent = `Due: ${dueDate}`;
+            dueDateContent.classList.add("due-date");
+            li.appendChild(dueDateContent);
+        }
+
+        if (note.length > 0) {
+            const noteContent = document.createElement("div");
+            noteContent.textContent = note;
+            noteContent.classList.add("notes");
+            li.appendChild(noteContent);
+        }
+
+        const deleteBtn = document.createElement("span");
+        deleteBtn.innerHTML = "&#x2715;"; // Unicode for the multiplication (X) character
+        deleteBtn.style.display = "none";
+        deleteBtn.addEventListener("click", () => {
+            if (editMode) {
+                li.remove();
+            }
+        });
+
+        li.appendChild(deleteBtn);
+        document.getElementById("todoList").appendChild(li);
+        input.value = "";
+        noteInput.value = "";
+
+        li.addEventListener("mouseover", () => {
+            if (editMode) {
+                deleteBtn.style.display = "inline";
+                li.setAttribute("draggable", "true");
+            } else {
+                deleteBtn.style.display = "none";
+                li.removeAttribute("draggable");
+            }
+        });
+
+        li.addEventListener("mouseout", () => {
+            deleteBtn.style.display = "none";
+        });
+
+        li.addEventListener("dragstart", handleDragStart);
+        li.addEventListener("dragover", handleDragOver);
+        li.addEventListener("dragleave", handleDragLeave);
+        li.addEventListener("drop", handleDrop);
+        li.addEventListener("dragend", handleDragEnd);
+    }
+}
+
+let draggedItem;
+
+function handleDragStart(e) {
+    if (editMode) {
+        this.classList.add('dragged');
+        draggedItem = this;
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+}
+
+function handleDragOver(e) {
+    if (editMode) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+
+        // Remove hovered class from all items
+        const items = document.querySelectorAll('#todoList li');
+        items.forEach(item => item.classList.remove('hovered'));
+
+        // Add hovered class to the current item
+        this.classList.add('hovered');
+        return false;
+    }
+}
+
+function handleDragLeave() {
+    if (editMode) {
+        // Remove hovered class
+        this.classList.remove('hovered');
+    }
+}
+
+function handleDrop(e) {
+    if (editMode) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
+
+        if (draggedItem !== this) {
+            draggedItem.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        // Remove hovered class
+        this.classList.remove('hovered');
+        return false;
+    }
+}
+
+function handleDragEnd() {
+    this.classList.remove('dragged');
+
+    // Remove hovered class from all items
+    const items = document.querySelectorAll('#todoList li');
+    items.forEach(item => item.classList.remove('hovered'));
+}
+
